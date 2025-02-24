08ae472b63f9b077c007f046cab23805b1d2f109
CalderonExe22 <mario.calderon@est.fi.uncoma.edu.ar>
2025-02-24 00:14:08 -0300

    .

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -4,7 +4,7 @@
         <form class="flex flex-col gap-10" action="{{ route('roles.store') }}" method="post">
             @csrf
             @include('roles.form-fields')
-            <button class="p-3 w-1/2 cursor-pointer rounded-3xl transition-colors bg-black text-white hover:bg-white hover:text-black hover:outline-2" type="submit">Crear usuario</button>
+            <button class="p-3 w-1/2 cursor-pointer rounded-3xl transition-colors bg-black text-white hover:bg-white hover:text-black hover:outline-2" type="submit">Crear rol</button>
         </form>
     </div>
 </x-layouts.app>
